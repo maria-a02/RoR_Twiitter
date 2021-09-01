59829ce0c685d2fb83653e06fa06df7d42989559
maria-a02 <maria.aramaki@gmail.com>
2021-08-31 20:02:46 -0400

    Follows controller fixed

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,7 +1,7 @@
 class FollowsController < ApplicationController
     def to_follow
         @following = User.find(params[:id])
-        new_follow 0 Follow.create!(follower: current_user, following: @following)
+        new_follow = Follow.create!(follower: current_user, following: @following)
         redirect_to root_path
     end
 end

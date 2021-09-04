7865a061b913979bc5611c5d6ebf49d71e023b7d
maria-a02 <maria.aramaki@gmail.com>
2021-09-04 16:21:02 -0400

    Seed changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin22@example.com', password: '123456', password_confirmation: '123456') if Rails.env.development?
+AdminUser.create!(email: 'admin22@example.com', password: '123456', password_confirmation: '123456') #if Rails.env.development?

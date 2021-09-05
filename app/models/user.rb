322b939ca059c1bacaec46e53bb9c6e4b6df19f0
maria-a02 <maria.aramaki@gmail.com>
2021-09-05 16:40:27 -0300

    Model user modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  has_many :tweets
-  has_many :likes
+  has_many :tweet, dependent: :destroy
+  has_many :likes, dependent: :destroy
   has_many :followers, class_name: 'Follow', foreign_key: :following_id, dependent: :destroy
   has_many :followings, class_name: 'Follow', foreign_key: :follower_id, dependent: :destroy
 

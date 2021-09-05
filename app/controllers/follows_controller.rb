f3cb8b0680ec5504042d7b8314c91dd18f73820c
maria-a02 <maria.aramaki@gmail.com>
2021-09-05 11:29:22 -0300

    Admin users modified

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,4 +1,5 @@
 class FollowsController < ApplicationController
+    before_action :authenticate_user!
     def to_follow
         @following = User.find(params[:id])
         new_follow = Follow.create!(following: current_user, follower: @following)

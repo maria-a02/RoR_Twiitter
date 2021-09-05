e642f57d282437ce4fa422c5a64db9e149e41bf5
maria-a02 <maria.aramaki@gmail.com>
2021-09-05 16:45:55 -0300

    Model user modified

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
   has_many :tweet, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  devise :database_authenticatable, :registerable,
+        :recoverable, :rememberable, :validatable
+
   has_many :followers, class_name: 'Follow', foreign_key: :following_id, dependent: :destroy
   has_many :followings, class_name: 'Follow', foreign_key: :follower_id, dependent: :destroy
 
